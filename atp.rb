c40dcf93b3eb4abbc617a5a52e3131b265d45900
U-yasu-VAIO\yasu <yasu@yasu-VAIO.(none)>
2011-07-01 21:43:57 +0900

    testing unicode chars

--- atp.rb
+++ atp.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
+
+# 2011 Hot Summer in Yugawara Baby
+# Yasuaki Kudo
+
+
+
 class Wff
 	@counter=0
 	attr_reader :counter
@@ -23,32 +29,45 @@ class NegationWff <  Wff
 	def who_am_i
 		'(n' + @counter.to_s + ' ' + @arg.who_am_i+')'
 	end
+	def visual 
+		'(Ê' + @counter.to_s + ' ' + @arg.visual+')'
+	end
 end
 class BinaryWff < Wff
 	attr_accessor :arg1,:arg2
 	class << self
 		attr_reader :designation
+		attr_reader :visual_designation
 	end
 	def initialize wff1, wff2
 		@arg1,@arg2 = wff1,wff2
 		super()
 	end
 	def who_am_i
-		'('+self.class.designation.to_s + @counter.to_s + ' ' + @arg1.who_am_i+ ',' + @arg2.who_am_i + ')'
+		'('+self.class.designation.to_s + @counter.to_s + ' ' + @arg1.who_am_i+ ',' + @arg2.who_am_i+ ')'
+	end
+	def visual 
+		'('+self.class.visual_designation + @counter.to_s + ' ' + @arg1.visual+ ',' + @arg2.visual+ ')'
 	end
 end
 class ConjunctionWff < BinaryWff
 	@designation=:c
+	@visual_designation='È'
 end
 class DisjunctionWff < BinaryWff
 	@designation=:d
+	@visual_designation='É'
 end
 class ImplicationWff < BinaryWff
 	@designation=:i
+	@visual_designation='¨'
 end
 
 class AtomicWff < Wff
 	def who_am_i
+		'p' + @counter.to_s
+	end
+	def visual 
 		'p' + @counter.to_s
 	end
 end
@@ -61,7 +80,16 @@ end
 def hello4
 	p 3.times.map{|x|DisjunctionWff.new(AtomicWff.new, AtomicWff.new)}.map{|x|x.who_am_i}
 end
-def hello5
-	p 3.times.map{|x|ImplicationWff.new(AtomicWff.new, AtomicWff.new)}.map{|x|x.who_am_i}
+def hello6
+	ImplicationWff.new(
+		AtomicWff.new,
+		DisjunctionWff.new(
+			AtomicWff.new,
+			AtomicWff.new
+			)
+		)
 end
-hello5
+h = hello6
+p h.who_am_i
+puts h.visual
+
